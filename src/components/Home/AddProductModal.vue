10b325e29fd3571e5c508437ad7fbced6674da34
khoatrannfq <dangtran.khoa@nfq.com>
2024-03-03 22:30:27 +0700

    fix: validation and close modal

--- src/components/Home/AddProductModal.vue
+++ src/components/Home/AddProductModal.vue
@@ -26,6 +26,12 @@ const addProduct = () => {
   emit('addProduct', newProduct)
   
   productInfo.value = { ...initProductInfo }
+
+  const closeModalButton = document.getElementById('close-add-product-modal')
+
+  if (closeModalButton) {
+    closeModalButton.click()
+  }
 }
 
 </script>
@@ -40,6 +46,7 @@ const addProduct = () => {
         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Thêm thuốc</h3>
           <button type="button"
+            id="close-add-product-modal"
             class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
             data-modal-hide="add-product-modal">
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -51,12 +58,13 @@ const addProduct = () => {
         </div>
         <!-- Modal body -->
         <div class="p-4 md:p-5">
-          <form class="space-y-4">
+          <form class="space-y-4" @submit.prevent="addProduct">
             <div>
               <label for="product-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Tên thuốc
               </label>
-              <input v-model="productInfo.name" type="product-name" name="product-name" id="product-name"
+              <input v-model="productInfo.name"
+                type="product-name" name="product-name" id="product-name"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                 placeholder="Paracetamol 650mg" required />
             </div>
@@ -75,7 +83,7 @@ const addProduct = () => {
               <input v-model="productInfo.quantity" type="product-quantity" name="product-quantity"
                 id="product-quantity"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                placeholder="Nhập số lượng" required />
+                placeholder="Nhập số lượng" />
             </div>
             <div>
               <label for="product-unit" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -83,11 +91,10 @@ const addProduct = () => {
               </label>
               <input v-model="productInfo.unit" type="product-unit" name="product-unit" id="product-unit"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                placeholder="Nhập số lượng" required />
+                placeholder="Nhập số lượng" />
             </div>
 
-            <button @click="addProduct" type="button"
-              data-modal-hide="add-product-modal"
+            <button type="submit"
               class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Thêm thuốc
             </button>
