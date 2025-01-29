1ca157340bc8407c694234b021d4869e99777e58
richardktran.dev <richardktran.dev@gmail.com>
2025-01-29 20:17:00 +0700

    change the way to display score

--- src/components/Home/UserScore.vue
+++ src/components/Home/UserScore.vue
@@ -22,20 +22,20 @@ const props = defineProps({
 <template>
   <dl class="grid grid-cols-1 gap-x-4 gap-y-4 sm:grid-cols-2">
     <div>
-      <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">Điểm tổng</dt>
-      <dd class="text-sm font-semibold text-gray-900 dark:text-white">{{ props.total_score }} điểm ({{ (props.total_score*1000).toLocaleString('en-US') }} VNĐ)</dd>
+      <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">Đã mua tổng cộng</dt>
+      <dd class="text-sm font-semibold text-gray-900 dark:text-white">{{ (props.total_score*1000).toLocaleString('en-US') }} VNĐ</dd>
     </div>
-    <div>
+    <!-- <div>
       <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">Điểm còn lại</dt>
-      <dd class="text-sm font-semibold text-gray-900 dark:text-white">{{ props.remain_score }} điểm ({{ (props.remain_score*1000).toLocaleString('en-US') }} VNĐ)</dd>
-    </div>
+      <dd class="text-sm font-semibold text-gray-900 dark:text-white">{{ props.remain_score }} điểm</dd>
+    </div> -->
     <div>
-      <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">Điểm đã sử dụng</dt>
+      <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">Điểm đã trả (Điểm cũ)</dt>
       <dd class="text-sm font-semibold text-gray-900 dark:text-white">{{ props.score_used }} điểm ({{ (props.score_used*1000).toLocaleString('en-US') }} VNĐ)</dd>
     </div>
     <div>
-      <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">Điểm sử dụng tối đa</dt>
-      <dd class="text-sm font-semibold text-gray-900 dark:text-white">{{ props.max_score }} điểm ({{ (props.max_score*1000).toLocaleString('en-US') }} VNĐ)</dd>
+      <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">Số tiền có thể giảm</dt>
+      <dd class="text-sm font-semibold text-gray-900 dark:text-white">{{ (props.max_score*1000).toLocaleString('en-US') }} VNĐ</dd>
     </div>
   </dl>
 </template>
